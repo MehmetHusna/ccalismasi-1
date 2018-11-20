563fa63e92b4afb46070bacb390d20502bcf3971
MehmetHusna <36952339+MehmetHusna@users.noreply.github.com>
2018-11-21 01:09:49 +0300

    Yorum satırları ve \n

---
+++ Yorum Satırları ve n  kullanımı.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+	            // YORUM SATIRI TANIMLAMA ÞEKLÝ
+	
+	
+	/* Yorum Satýrý */  //(/*...*/) 
+	// Yorum Satýrý 2. kullaným þekli (//...)
+	
+		
+				// \n Kullanýmý
+		
+		printf("Mehmet\nHüsna\nKýþla");
+		
+	
+	
+return 0;	
+}
